d1d661f59fee9961235b5742cfb9f90ff4c1aa07
gaebaribari <gaebaribari@gmail.com>
2024-09-19 18:19:45 +0900

    Practice: handling Error & Loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { AppForm } from './AppForm';
+import AppProducts from './AppProducts';
 
 function App() {
   return (
     <div className="App">
-      <AppForm/>
+      <AppProducts />
     </div>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,26 +3,35 @@ import React, { useEffect, useState } from 'react';
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleChange = () => setChecked((prev) => !prev);
 
+  const getProductsNetwork = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`data/${checked ? 'sale_' : ''}products.json`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+          setProducts(data);
+        });
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   useEffect(() => {
-    fetch(`data/${checked ? 'sale_' : ''}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
-      });
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
+    getProductsNetwork();
   }, [checked]);
 
+  if (error) return <ErrorPage />
+  if (loading) return <Loading />
   return (
     <>
-      <input id='checkbox' type='checkbox' value={checked} onChange={handleChange} />
-      <label htmlFor="checkbox">세일 제품 보여주기</label>
-      <ul>
+      <div><ul>
         {products.map((product) => (
           <li key={product.id}>
             <article>
@@ -31,7 +40,21 @@ export default function Products() {
             </article>
           </li>
         ))}
-      </ul>
+      </ul></div>
+      <input id='checkbox' type='checkbox' value={checked} onChange={handleChange} />
+      <label htmlFor="checkbox">세일 제품 보여주기</label>
     </>
+  );
+}
+
+function Loading() {
+  return (
+    <span>로딩중입니다</span>
+  );
+}
+
+function ErrorPage() {
+  return (
+    <span>에러 🤬</span>
   );
 }
